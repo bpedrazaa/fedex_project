58eab6eb9d4ad02625166e5b06e491bb60a78c3d
EC2 Default User <ec2-user@ip-172-31-11-252.us-east-2.compute.internal>
2021-06-26 22:43:50 +0000

    fdx-03 Functions to change state of the package and send message to the customer completed

--- src/fedex_requests.py
+++ src/fedex_requests.py
@@ -15,6 +15,9 @@ table = dynamodb.Table(fedex_table)
 sns = boto3.resource('sns')
 topic = sns.Topic(fedex_topic_arn)
 
+# /////////////////////////////////////////////////////////////////////////////
+
+# Function to change state of package to embarked
 def fedexPutEmbarked(event, context):
     
     # Get the package id from the path
@@ -23,31 +26,128 @@ def fedexPutEmbarked(event, context):
     package_id = array_path[-2]
     
     # Get the info of the package
-    #response = table.get_item(
-    #    Key={
-    #        'pk': package_id,
-    #        'sk': package_id
-    #    }
-    #)
-    # Verify if the response is not empty, send error message or not
-    #item = response['Item']
+    response = table.get_item(
+        Key={
+            'pk': package_id,
+            'sk': package_id
+        }
+    )
     
-    # Verify if the last state of the package
+    # Verify if the response is not empty, send error message if is empty
+    return_value = ""
+    if 'Item' in response:
+        item = response['Item']
+        
+        # Verify the last state of the package
+        packaged_state = item['packaged']
+        if packaged_state == "1" or packaged_state == 1:
+            
+            # UPDATE the state of the package
+            table.update_item(
+                Key={
+                    'pk': package_id,
+                    'sk': package_id
+                },
+                # Update the previous state and change to the new state
+                UpdateExpression='SET packaged = :packaged_state, embarked = :embarked_state',
+                ExpressionAttributeValues={
+                    ':packaged_state': '0',
+                    ':embarked_state': '1'
+                }
+            )
+            return_value = "State of " + package_id + " updated to embarked!"
+
+            # //    Send a message to the client     //
+            message = "Dear Customer, your package with the identifier: ***" + package_id + "*** is currently in the EMBARKED state. We will keep you informed"
+                 
+            response_topic = topic.publish(
+                Message=message,
+                MessageAttributes={
+                    'package_id': {
+                        'DataType': 'String',
+                        'StringValue': str(package_id)
+                    }
+                }
+            )
+            
+            return_value = return_value + ", message of state of package sended to the customer"
+                
+        else:
+            return_value = "Bad formation of states of package, the last state should be packaged"
+    else:
+        return_value = "Item not found in the fedex-table"
+
     
-    # UPDATE the state of the package
-    
-    # Get the subscriber (email of customer) of the package (work with Thomas, get customer item)
-    
-    
-    # //    Send a message to the client     //
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps(return_value)
     }
     
+    
+
+
+# Function to change state of package to Routed
 def fedexPutRouted(event, context):
-    print(json.dumps({"running": True}))
+    
+    # Get the package id from the path
+    path = event["path"]
+    array_path = path.split("/")
+    package_id = array_path[-2]
+    
+    # Get the info of the package
+    response = table.get_item(
+        Key={
+            'pk': package_id,
+            'sk': package_id
+        }
+    )
+    
+    # Verify if the response is not empty, send error message if is empty
+    return_value = ""
+    if 'Item' in response:
+        item = response['Item']
+        
+        # Verify the last state of the package
+        embarked_state = item['embarked']
+        if embarked_state == "1" or embarked_state == 1:
+            
+            # UPDATE the state of the package
+            table.update_item(
+                Key={
+                    'pk': package_id,
+                    'sk': package_id
+                },
+                # Update the previous state and change to the new state
+                UpdateExpression='SET embarked = :embarked_state, routed = :routed_state',
+                ExpressionAttributeValues={
+                    ':embarked_state': '0',
+                    ':routed_state': '1'
+                }
+            )
+            return_value = "State of " + package_id + " updated to routed!"
+
+            # //    Send a message to the client     //
+            message = "Dear Customer, your package with the identifier: ***" + package_id + "*** is currently in the ROUTED state. We will keep you informed"
+                 
+            response_topic = topic.publish(
+                Message=message,
+                MessageAttributes={
+                    'package_id': {
+                        'DataType': 'String',
+                        'StringValue': str(package_id)
+                    }
+                }
+            )
+            
+            return_value = return_value + ", message of state of package sended to the customer"
+                
+        else:
+            return_value = "Bad formation of states of package, the last state should be embarked"
+    else:
+        return_value = "Item not found in the fedex-table"
+
+    
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps(return_value)
     }
