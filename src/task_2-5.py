cbe8eafbdafddc01c712d2287c511ac24a17bbb2
EC2 Default User <ec2-user@ip-172-31-53-162.ec2.internal>
2021-06-29 21:08:27 +0000

    Added API Distance

--- src/task_2-5.py
+++ src/task_2-5.py
@@ -3,6 +3,7 @@ import boto3
 import os
 from boto3.dynamodb.conditions import Key, Attr
 from botocore.exceptions import ClientError
+import requests
 
 # Environment Variables
 fedex_table = os.environ['FEDEX_TABLE']
@@ -76,17 +77,69 @@ def fedexPutDiscount(event, context):
         
     # Price Calculation
     
-    ########### aquí lo de Distance ###########
+    # Get package from table
     
-    # d = something
+    response = table.get_item(
+        Key={
+            'pk': package_id,
+            'sk': package_id
+        }
+    )
+    package = response['Item']
+    # Cities origin and destination
+    origin = package['origin']
+    destination = package['destination']
+    
+    # First we search in DB for distance
+    response = table.get_item(
+        Key={
+            'pk': f'city_{origin}',
+            'sk': f'city_{destination}'
+        }
+    )
+    if "Item" in response: # If we had already saved the distance we return it
+        item = response['Item']
+        distance = item['distance']
+        distance = int(distance)
+    else:
+        # Get req to Distance API
+        url = f'https://www.distance24.org/route.json?stops={origin}|{destination}'
+        distance_info = requests.get(url)
+        distance_json = distance_info.json()
+        
+        # Distance (in KM)
+        distance = distance_json['distance']
+        
+        #Save distance in Cache (DB)
+        table.put_item(
+                Item={
+                    'pk': f'city_{origin}',
+                    'sk': f'city_{destination}',
+                    'distance': str(distance)
+                }
+            )
+    
+    d = distance
     w = package_weight
     v = package_dimensions
     
-    p = max(w*0.5, v/1000)
-    #p  = (d/100)+max(w*0.5, v/1000)
+    #p = max(w*0.5, v/1000)
+    p  = (d/100)+max(w*0.5, v/1000)
     
     # Adding discount
     estimated_price = p - ((p/100)*final_discount) 
+    
+    # Update Price
+    table.update_item(
+        Key={
+            'pk': package_id,
+            'sk': package_id
+        },
+        UpdateExpression='SET estimated_price = :val1',
+        ExpressionAttributeValues={
+            ':val1': str(estimated_price)
+        }
+    )
     
     return {
         'statusCode': 200,
